625d02c0ef54fba206cdf77f26ece64cb903ebc4
Renato Veneroso <rveneroso@gmail.com>
2024-06-15 14:57:42 -0300

    Versão do site de acordo com o capítulo 05 - Uso de componentes

--- editora-maujortec/src/App.js
+++ editora-maujortec/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import React from "react";
+import React from 'react'
+import Topo from "./components/Topo";
+import Principal from './components/Principal';
+import Rodape from './components/Rodape';
 
-function App() {
+const App = () => {
   return (
     <>
-      <header className="topo">
-        <h1 className="logo"></h1>
-        <ul>
-          <li href="">Navegação</li>
-        </ul>
-      </header>
-      <main className="principal">
-        <h2>Últimos lançamentos</h2>
-        <div className="card">Card</div>
-      </main>
-      <footer className="rodape">
-        <p>Conteúdos cedidos pela Editora Novatech &#8212; Copyright 2020</p>
-      </footer>
+      <Topo />
+      <Principal />
+      <Rodape />
     </>
   );
 }
---
+++ editora-maujortec/src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+const Logo = () => {
+    return (
+        <a href="#">
+            <h1 className="logo">
+                <span>Editora MaujourTec</span>
+            </h1>
+        </a>
+    );
+}
+
+export default Logo;
